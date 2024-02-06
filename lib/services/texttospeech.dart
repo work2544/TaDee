f9a54b05a7d0094e81d5bfa138325c68a254b36a
work2544 <work21_123@hotmail.com>
2024-02-06 17:40:27 +0700

    singleton for tts

---
+++ lib/services/texttospeech.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class TextToSpeech {
+  static final TextToSpeech _instance = TextToSpeech._internal();
+  final double volume = 1;
+  final double pitch = 1.0;
+  final double rate = 0.5;
+  final String language = 'th-TH';
+  final String engine = 'com.google.android.tts';
+  late FlutterTts flutterTts;
+
+  factory TextToSpeech() {
+    return _instance;
+  }
+
+  TextToSpeech._internal() {
+    // initialization logic
+    flutterTts = FlutterTts();
+    _setAwaitOptions();
+    flutterTts.setLanguage(language);
+    flutterTts.setEngine(engine);
+    flutterTts.setInitHandler(() {});
+    flutterTts.setVolume(volume);
+    flutterTts.setSpeechRate(rate);
+    flutterTts.setPitch(pitch);
+  }
+
+  Future _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  Future<void> callSpeak(String newVoiceText) async {
+    if (newVoiceText.isNotEmpty) {
+      await flutterTts.speak(newVoiceText);
+    }
+  }
+
+  void speak(String newVoiceText) {
+    callSpeak(newVoiceText);
+  }
+
+  void stop() {
+    flutterTts.stop();
+  }
+}
